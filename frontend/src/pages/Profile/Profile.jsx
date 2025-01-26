7a7dd81897cfd56f7c442ba8317c3c244364e50e
Paz Kohnenkampf <paz.kohnenkampf@usach.cl>
2025-01-26 20:36:26 -0300

    logout in profile

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, {useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { Container, Button } from "react-bootstrap";
+import { UserContext } from '../../context/UserContext'
 
 const Profile = () => {
+  const { logout } = useContext(UserContext)
   return (
     <Container fluid="md" className='d-flex flex-wrap justify-content-md-center'>
         <h1 style={{width: "100%"}}>Tu perfil</h1>
         <h2 style={{width: "100%"}}>Usuario: usuariodemo@dl.com</h2>
         <div style={{width: "100%"}}>
             <Link to='/'>
-                <Button variant='outline-primary' >🔒 Logout</Button>
+                <Button variant='outline-primary' onClick={logout}>🔒 Logout</Button>
             </Link>
         </div>
     </Container>
